0a409539cb6a212b3fe924c27fbeb054a30a2db9
levan <levan.martirosyan@bk.ru>
2024-12-04 18:02:51 +0400

    fixed bugs

--- src/app/home/productsection/productsection.component.ts
+++ src/app/home/productsection/productsection.component.ts
@@ -105,7 +105,7 @@ export class ProductsectionComponent implements OnInit {
   scrollLeft() {
     const container = this.productContainer.nativeElement;
     container.scrollBy({
-      left: -380,
+      left: -350,
       behavior: 'smooth',
     });
   }
@@ -113,7 +113,7 @@ export class ProductsectionComponent implements OnInit {
   scrollRight() {
     const container = this.productContainer.nativeElement;
     container.scrollBy({
-      left: 380,
+      left: 350,
       behavior: 'smooth',
     });
   }
