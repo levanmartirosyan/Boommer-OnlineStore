2446bf33e028536a4dd1d7f85ad366f4f6b8d018
levan <levan.martirosyan@bk.ru>
2024-12-13 00:51:01 +0400

    fixed bugs, changed product filter system, changed top section slider

--- src/app/home/topsection/topsection.component.ts
+++ src/app/home/topsection/topsection.component.ts
@@ -13,18 +13,38 @@ export class TopsectionComponent implements OnInit {
 
   ngOnInit(): void {
     this.showCategories();
+    this.getBanner();
   }
+  public bannerImages: any;
 
   images: string[] = [
-    'https://zoommer.ge/_next/image?url=https%3A%2F%2Fs3.zoommer.ge%2Fsite%2Fb161174e-a8c6-413b-bbea-32bbfe31937e_Thumb.jpeg&w=1920&q=75',
-    'https://zoommer.ge/_next/image?url=https%3A%2F%2Fs3.zoommer.ge%2Fsite%2Fcf92163a-d8dd-48e4-b725-b500f51427a8_Thumb.jpeg&w=1920&q=75',
-    'https://zoommer.ge/_next/image?url=https%3A%2F%2Fs3.zoommer.ge%2Fsite%2F5bc25c6c-ac14-4f5b-a23b-5ad95b36e2a5_Thumb.jpeg&w=1920&q=75',
-    'https://zoommer.ge/_next/image?url=https%3A%2F%2Fs3.zoommer.ge%2Fsite%2F03667919-46b1-443d-8e8a-d8fa771cb41d_Thumb.jpeg&w=1920&q=75',
-    'https://zoommer.ge/_next/image?url=https%3A%2F%2Fs3.zoommer.ge%2Fsite%2Fedaa0688-e2f4-4aea-892a-1e9a3d2a4631_Thumb.jpeg&w=1920&q=75',
-    'https://zoommer.ge/_next/image?url=https%3A%2F%2Fs3.zoommer.ge%2Fsite%2F9ae8bf14-9283-47ac-b8be-1a9ce79bcc38_Thumb.jpeg&w=1920&q=75',
-    'https://zoommer.ge/_next/image?url=https%3A%2F%2Fs3.zoommer.ge%2Fsite%2Fe3336fe9-2638-415a-91a8-32d67a39517b_Thumb.jpeg&w=1920&q=75',
-    'https://zoommer.ge/_next/image?url=https%3A%2F%2Fs3.zoommer.ge%2Fsite%2F8fe8198e-b2c3-4bc1-b47b-e44842338cc2_Thumb.jpeg&w=1920&q=75',
+    // 'https://zoommer.ge/_next/image?url=https%3A%2F%2Fs3.zoommer.ge%2Fsite%2Fb161174e-a8c6-413b-bbea-32bbfe31937e_Thumb.jpeg&w=1920&q=75',
+    // 'https://zoommer.ge/_next/image?url=https%3A%2F%2Fs3.zoommer.ge%2Fsite%2Fcf92163a-d8dd-48e4-b725-b500f51427a8_Thumb.jpeg&w=1920&q=75',
+    // 'https://zoommer.ge/_next/image?url=https%3A%2F%2Fs3.zoommer.ge%2Fsite%2F5bc25c6c-ac14-4f5b-a23b-5ad95b36e2a5_Thumb.jpeg&w=1920&q=75',
+    // 'https://zoommer.ge/_next/image?url=https%3A%2F%2Fs3.zoommer.ge%2Fsite%2F03667919-46b1-443d-8e8a-d8fa771cb41d_Thumb.jpeg&w=1920&q=75',
+    // 'https://zoommer.ge/_next/image?url=https%3A%2F%2Fs3.zoommer.ge%2Fsite%2Fedaa0688-e2f4-4aea-892a-1e9a3d2a4631_Thumb.jpeg&w=1920&q=75',
+    // 'https://zoommer.ge/_next/image?url=https%3A%2F%2Fs3.zoommer.ge%2Fsite%2F9ae8bf14-9283-47ac-b8be-1a9ce79bcc38_Thumb.jpeg&w=1920&q=75',
+    // 'https://zoommer.ge/_next/image?url=https%3A%2F%2Fs3.zoommer.ge%2Fsite%2Fe3336fe9-2638-415a-91a8-32d67a39517b_Thumb.jpeg&w=1920&q=75',
+    // 'https://zoommer.ge/_next/image?url=https%3A%2F%2Fs3.zoommer.ge%2Fsite%2F8fe8198e-b2c3-4bc1-b47b-e44842338cc2_Thumb.jpeg&w=1920&q=75',
   ];
+
+  imagesM: string[] = [];
+
+  getBanner() {
+    this.apiService.zoommerBanner().subscribe({
+      next: (data: any) => {
+        console.log(data.section);
+        this.bannerImages = data.section;
+        for (const item of this.bannerImages[0].banners) {
+          this.images.push(item.webImageUrl);
+          this.imagesM.push(item.mobileImageUrl);
+        }
+      },
+      error: (error: any) => {
+        console.log(error);
+      },
+    });
+  }
 
   public currentIndex = 0;
 
